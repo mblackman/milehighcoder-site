282e30a160af35939c22de016b12400aad7ccd7e
Matt Blackman <mateoblackman@gmail.com>
2024-12-07 11:56:28 -0700

    Organize posts under article subdir

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,5 +1,6 @@
 export default {
   // Set a default layout for everything in the src folder and below.
   layout: "layouts/post.liquid",
-  permalink: "{{ page.fileSlug }}/index.html",
+  permalink: "article/{{ page.fileSlug }}/index.html",
+  tags: ["post"],
 };
