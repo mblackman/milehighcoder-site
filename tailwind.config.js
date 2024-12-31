ebfda7a7e3d29532f2eb1bd14d8637f289c3b3a2
Matt Blackman <mateoblackman@gmail.com>
2024-12-31 14:14:30 -0700

    Fix page width

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./src/**/*.{html,liquid,njk}", "./.eleventy.js"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100ch',
+          },
+        },
+      },
+    },
   },
   daisyui: {
     themes: ["retro", "night"],
